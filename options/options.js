b5afd1daf4e7a98a40a007f6c4eb813f352058db
Crowdbotics <team@crowdbotics.com>
2021-09-18 20:12:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "manylingue_30662Identifier",
+  url: "https://manylingue_30662Identifier.botics.co",
+  api: "https://manylingue_30662Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
